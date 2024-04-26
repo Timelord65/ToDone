42cd9143a6436ab881e0e2947a18af52236cebde
Timelorde <praneethchokka@gmail.com>
2024-04-27 02:35:34 +0530

    Adding other config related stuff

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,22 @@
 import express from "express";
 import config from './config/config.json' assert {type: 'json'}
 
-const app = express();
-const PORT = config.PORT.val;
 
+const app = express();
+
+const PORT = config.PORT.val;
 
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
+});
+
+app.use((req, res, next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+
+app.get("/test", (req, res) => {
+    console.log("Test successfull. Connection to server is possible.");
+    res.send("Test called successfully");
 });
